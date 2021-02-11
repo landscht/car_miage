d6041ab3bb0c598612ea7b52d0d835ff894dd550
gabrielcongin <gabriel.congin@gmail.com>
2021-02-11 14:46:23 +0100

    feat(front) : add router and header

---
+++ tp2/tp2_front/src/screens/HomeScreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="padding-top: 50px" align="center">
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
