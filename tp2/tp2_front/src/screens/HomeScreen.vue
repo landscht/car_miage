88cf23318cd98c43865b0c78960286227c0e98ef
gabrielcongin <gabriel.congin@gmail.com>
2021-02-18 12:04:59 +0100

    feat : add home image

--- tp2/tp2_front/src/screens/HomeScreen.vue
+++ tp2/tp2_front/src/screens/HomeScreen.vue
@@ -1,15 +1,18 @@
 <template>
   <div style="padding-top: 50px" align="center">
-    <h1>Welcome</h1>
+    <h1 style="padding-bottom: 15px">Bienvenue sur Amazon!</h1>
+    <v-img
+        src="../assets/online_shopping.jpg"
+        max-height="500px"
+        max-width="1000px"
+    >
+    </v-img>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home",
-  /*mounted() {
-    localStorage.clear();
-  }*/
+  name: "Home"
 }
 </script>
 
